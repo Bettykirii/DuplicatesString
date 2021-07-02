29889fc9dbcce81af40e01995312b1a5840124cf
Warware_kirii <warwarekirii@gmail.com>
2021-07-02 13:50:27 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import static com.company.DuplicateString.removeDuplicates;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String s = "abbaca";
+        System.out.println(removeDuplicates( s));
+    }
+}
